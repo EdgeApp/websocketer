08d72d40debf93d8bbfa959fcf39e0a0d1e3a399
Paul Puey <paul@edge.app>
2018-10-13 22:31:57 -0700

    Add multiple server support with rotation

--- src/indexWebsocketer.js
+++ src/indexWebsocketer.js
@@ -27,6 +27,7 @@ const wss = new WebSocket.Server({
 })
 
 const _connections = {}
+const _tcpServers = CONFIG.tcpServers.slice()
 
 wss.on('connection', (ws, req) => {
   const address = req.connection.remoteAddress
@@ -49,30 +50,63 @@ class Connection {
   ws: Object
   client: net.Socket
   address: string
+  retry: boolean
+  canRetry: boolean
+  numServers: number
 
   constructor (ws, addrPort) {
     this.ws = ws
     this.client = new net.Socket()
     this.client.setEncoding('utf8')
     this.address = addrPort
+    this.numServers = _tcpServers.length
+    this.retry = false
+    this.canRetry = true
+  }
+
+  connectServer () {
+    if (this.numServers) {
+      this.numServers--
+      const tcpServer = _tcpServers[0]
+      const host = tcpServer.split(':')[0]
+      const port = tcpServer.split(':')[1]
+      this.client.connect({host, port}, (error) => {
+        console.log('Connected', error)
+      })
+    } else {
+      deleteConnection(this.address)
+    }
   }
 
   init () {
-    const tcpServer = CONFIG.tcpServers[0]
-    const host = tcpServer.split(':')[0]
-    const port = tcpServer.split(':')[1]
-    this.client.connect({host, port}, () => {
-      console.log('Connected')
+    this.client.on('close', (error) => {
+      console.log(error)
+      if (this.retry) {
+        this.retry = false
+        const bad = _tcpServers.splice(0, 1)
+        _tcpServers.push(bad[0])
+        this.connectServer()
+      } else {
+        deleteConnection(this.address)
+      }
     })
-
-    this.client.on('close', () => {
-      deleteConnection(this.address)
-    })
-    this.client.on('error', () => {
-      deleteConnection(this.address)
+    this.client.on('error', (error) => {
+      console.log(error)
+      if (
+        (
+          error.code === 'ECONNREFUSED' ||
+          error.code === 'ETIMEDOUT' ||
+          error.code === 'ENOTFOUND'
+        ) && this.canRetry
+      ) {
+        this.retry = true
+      } else {
+        deleteConnection(this.address)
+      }
     })
 
     this.client.on('data', (data) => {
+      this.canRetry = false
       this.ws.send(data, (error) => {
         if (error) {
           console.log('Error sending data to WS')
@@ -83,6 +117,7 @@ class Connection {
     })
 
     this.ws.on('message', (message) => {
+      this.canRetry = false
       this.client.write(message + '\r\n', 'utf8', (error) => {
         if (error) {
           console.log('Error writing to WS')
@@ -92,5 +127,14 @@ class Connection {
         }
       })
     })
+    this.ws.on('close', (error) => {
+      console.log('Websocket close', error)
+      deleteConnection(this.address)
+    })
+    this.ws.on('error', (error) => {
+      console.log('Websocket error', error)
+      deleteConnection(this.address)
+    })
+    this.connectServer()
   }
 }
